65b4a7796da444f99708f79a2760891da136050c
mlupm <manuel.leirafencing@gmail.com>
2021-11-05 09:30:35 +0100

    creado el proyecto maven #14 duracion 45 mins

---
+++ src/main/java/es/upm/profundizacion/App.java
@@ -0,0 +1,13 @@
+package es.upm.profundizacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
